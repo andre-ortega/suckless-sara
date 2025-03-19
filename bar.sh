2a8e8c9f79214978f560a7351b8b0ebb4a0c3ccc
hakirot <hackirot@proton.me>
2025-03-18 22:57:12 -0700

    new final working nonglitchy bars yay

--- bar.sh
+++ bar.sh
@@ -6,7 +6,12 @@ killall polybar
 # Wait untill the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch the bar
-polybar left --config=$HOME/.config/polybar/config.ini &
-polybar center --config=$HOME/.config/polybar/config.ini &
-polybar right --config=$HOME/.config/polybar/config.ini &
+# Launch bar for primary monitor
+#MONITOR=eDP-1 polybar left --config=$HOME/.config/polybar/config.ini &
+MONITOR=eDP-1 polybar center --config=$HOME/.config/polybar/config.ini &
+#MONITOR=eDP-1 polybar right --config=$HOME/.config/polybar/config.ini &
+
+# Launch bar for secondary monitor
+#MONITOR=HDMI-1 polybar secondary-left --config=$HOME/.config/polybar/config.ini &
+MONITOR=HDMI-1 polybar secondary-center --config=$HOME/.config/polybar/config.ini &
+#MONITOR=HDMI-1 polybar secondary-right --config=$HOME/.config/polybar/config.ini &
