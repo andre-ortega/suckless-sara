6eea3d0d5188a734e8b324da169b86e9a64ec7c1
hakirot <hackirot@proton.me>
2025-03-20 21:55:52 -0700

    sleeker focused borders

--- dwm/config.h
+++ dwm/config.h
@@ -2,7 +2,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 0;        /* border pixel of windows */
+static const unsigned int borderpx  = 2;        /* border pixel of windows */
 static const unsigned int gappx     = 0;       /* gap pixel between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
