fbaaa3f77174f492d2f3be02ece399826c5bae2c
hakirot <hackirot@proton.me>
2025-11-06 19:44:27 -0800

    reinstate dwm incmaster

--- dmenu/colors-wal-dmenu.h
+++ dmenu/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#e3d3de", "#060709" },
-	[SchemeSel] = { "#060709", "#adb8c4" },
+	[SchemeSel] = { "#060709", "#c7adbd" },
 	[SchemeOut] = { "#e3d3de", "#060709" },
 };
