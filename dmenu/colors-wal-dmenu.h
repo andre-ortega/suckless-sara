71aafb865fdf5ac97e01b0d2be162c15386fd3b8
hakirot <hackirot@proton.me>
2026-02-25 16:53:19 -0800

    oh-my-zsh diff

--- dmenu/colors-wal-dmenu.h
+++ dmenu/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#d4d7b9", "#3d383d" },
-	[SchemeSel] = { "#d4d7b9", "#add1ca" },
-	[SchemeOut] = { "#d4d7b9", "#886553" },
+	[SchemeNorm] = { "#cad5d7", "#2d323d" },
+	[SchemeSel] = { "#cad5d7", "#8896e0" },
+	[SchemeOut] = { "#cad5d7", "#576B67" },
 };
