6223b5dddecf37a2710efc9568d79e1463d0b219
hakirot <hackirot@proton.me>
2026-08-17 22:34:42 -0700

    path change

--- dwm/config.h
+++ dwm/config.h
@@ -28,7 +28,7 @@ static const unsigned int borderalpha = OPAQUE;
 /*	[SchemeSel]  = { col_gray4, col_gray2,  col_cyan  },*/
 /*};*/
 
-#include "/home/roe/git/suckless-hakirot/dwm/colors-wal-dwm.h"
+#include "colors-wal-dwm.h"
 
 static const unsigned int alphas[][3]      = {
     /*               fg      bg        border*/
