7d5f747ab88e5d5d7a96e3905f005f3169c49431
hakirot <hackirot@proton.me>
2026-08-23 03:13:59 -0700

    updates

--- dwm/config.h
+++ dwm/config.h
@@ -3,7 +3,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 0;       /* gap pixel between windows */
+static const unsigned int gappx     = 12;       /* gap pixel between windows */
 static const unsigned int snap      = 16;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
