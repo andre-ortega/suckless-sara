da00de4802aa09a40344de897ba26effd9691733
hakirot <hackirot@proton.me>
2026-09-02 00:08:13 -0700

    updates

--- dmenu/colors-wal-dmenu.h
+++ dmenu/colors-wal-dmenu.h
@@ -1,6 +1,6 @@
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#565453", "#232326" },
-	[SchemeSel] = { "#565453", "#93836d" },
-	[SchemeOut] = { "#565453", "#514e49" },
+	[SchemeNorm] = { "#c5c5c1", "#171709" },
+	[SchemeSel] = { "#c5c5c1", "#586068" },
+	[SchemeOut] = { "#c5c5c1", "#868F98" },
 };
